3bdc7426b482355d5a2a57bba14a63669cf36b3d
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-05-12 16:18:39 -0400

    add animation to my bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -4,7 +4,9 @@ const Bio = () => {
       <img src="./images/my-pict.JPG" alt="mypict" className="bio-photo" />
       <aside className="bio-block"></aside>
       <aside className="bio-description">
-        <h1>Hello, My name is Fransiskus Aldo</h1>
+        <h1 className="title">Hello, My name is Fransiskus Aldo.</h1>
+        <p className="p-developer">I am a web developer</p>
+        <p className="p-welcome">Welcome to my website</p>
       </aside>
     </section>
   );
