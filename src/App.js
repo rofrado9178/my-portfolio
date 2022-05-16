ee02c25f209f8846cecf2cc26c330c74a2c425a6
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-05-16 14:40:43 -0400

    add mock data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Bio from "./components/Bio";
+import data from "./data";
 
 function App() {
+  const projects = data;
   return (
     <main className="App">
       <section className="background"></section>
       <Bio />
+      <img src={projects[0].image[0]} />
     </main>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,102 @@
+const data = [
+  {
+    id: 1,
+    name: "Haime",
+    description:
+      "Haime is an Online Store App. It's a full-stack application with React as a front-end and Python Django as a back-end REST API",
+    github: "https://github.com/rofrado9178/online_store",
+    image: [],
+  },
+  {
+    id: 2,
+    name: "EZ-League",
+    description:
+      "Collaboration Project with my 2 other colleagues. Ez-League is a Sports League manager App. with React Front End, Tailwind CSS, Express JS, and PostgreSQL",
+    github: "https://github.com/jjjjjjonathan/ez-league",
+    image: [
+      "./images/ez1.png",
+      "./images/ez2.png",
+      "./images/ez3.png",
+      "./images/ez4.png",
+      "./images/ez5.png",
+      "./images/ez6.png",
+      "./images/ez7.png",
+    ],
+  },
+  {
+    id: 3,
+    name: "Interview Scheduler",
+    description:
+      "Interview Scheduler is a single-page React app for booking and also scheduling the interview.",
+    github: "https://github.com/rofrado9178/scheduler",
+    image: [
+      "./images/scheduler1.png",
+      "./images/scheduler2.png",
+      "./images/scheduler3.png",
+      "./images/scheduler4.png",
+    ],
+  },
+  {
+    id: 4,
+    name: "Spacetagram",
+    description:
+      "Simple Apps for sharing NASA Photo API, for Front end training purpose using React, axios",
+    github: "https://github.com/rofrado9178/NASA",
+    image: [
+      "./images/space1.png",
+      "./images/space2.png",
+      "./images/space3.png",
+      "./images/space4.png",
+      "./images/space5.png",
+    ],
+  },
+  {
+    id: 5,
+    name: "Jungle-Project",
+    description:
+      "Its an E-Commerce App. Legacy Code with Ruby on Rails, for Learn, debugging, and adding a new feature.",
+    github: "https://github.com/rofrado9178/Jungle-Project",
+    image: [
+      "./images/jungle1.png",
+      "./images/jungle2.png",
+      "./images/jungle3.png",
+      "./images/jungle4.png",
+      "./images/jungle5.png",
+      "./images/jungle6.png",
+      "./images/jungle7.png",
+      "./images/jungle8.png",
+    ],
+  },
+  {
+    id: 6,
+    name: "Once Upon A Book",
+    description:
+      "Once Upon A Book is a bookstore website. Collaboration Project with my 2 other colleagues. this App build with Express JS and EJS  with PostgreSQL",
+    github: "https://github.com/jjjjjjonathan/better-best-book-store",
+    image: ["./images/book1.png", "./images/book2.png", "./images/book3.png"],
+  },
+  {
+    id: 7,
+    name: "Tweeter Clone",
+    description:
+      "Tweeter Clone is a single page front-end project, with HTML, CSS, JQuery, AJAX, Node, Express",
+    github: "https://github.com/rofrado9178/tweeter",
+    image: [
+      "./images/tweet1.png",
+      "./images/tweet2.png",
+      "./images/tweet3.png",
+      "./images/tweet4.png",
+      "./images/tweet5.png",
+    ],
+  },
+  {
+    id: 8,
+    name: "Tiny Apps",
+    description:
+      "TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly).",
+    github: "https://github.com/rofrado9178/tinyapp",
+    image: ["./images/tiny1.png", "./images/tiny2.png", "./images/tiny3.png"],
+  },
+];
+
+export default data;
