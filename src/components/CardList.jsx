9539d917270f48d800c1f0f98c87b75928256c51
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-05-19 11:55:08 -0400

    add title and description in card projects

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -5,11 +5,15 @@ const CardList = ({ data }) => {
   const carouselItems = data.map((project) => {
     return (
       <article className="cardContainer">
+        <h1 className="project-title">{project.name}</h1>
         <CarouselContainer {...project} key={project.id} />
         <aside className="project-link">
           <a href={project.github} target="_blank" rel="noreferrer">
             <FaGithub className="github"> </FaGithub>
           </a>
+        </aside>
+        <aside>
+          <p className="project-description">{project.description}</p>
         </aside>
       </article>
     );
